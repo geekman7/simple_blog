ec85639e55b109d942d9a7834e5ea02a3bcf69af
Souleymane Guindo <souleymane.guindo15@gmail.com>
2020-04-13 19:40:43 +0100

    project updated

--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -14,5 +14,8 @@ urlpatterns = [
     path('profile/',users_views.profile, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]   
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
